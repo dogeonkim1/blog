b5835f57b77cb8b94f91545b3a41206b4d4241b3
dogeonkim1 <caddibo4@naver.com>
2025-03-09 23:32:11 +0900

    git test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          git test
+          git test again
         </p>
         <a
           className="App-link"
